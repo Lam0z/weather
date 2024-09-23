df9564da5b4509ac77f1c20288e95b5fd8f8f7c0
Nick Efimov <lamoz760@gmail.com>
2024-09-23 11:06:42 +0300

    add footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,19 @@
 <script setup></script>
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <div class="footer__container d-grid d-md-flex justify-content-between">
+            <span> For informational purposes </span>
+            <span>
+                Created by
+                <a
+                    href="https://t.me/Lam00z"
+                    class="text-decoration-none text-primary"
+                >
+                    @Nick</a
+                >
+            </span>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
